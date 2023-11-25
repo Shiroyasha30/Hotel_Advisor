2c65ca3f0cfe6f67fd02534feacaad0a43356efd
Shiroyasha30 <yash3.0upadhyay@gmail.com>
2023-11-26 01:21:53 +0530

    docker settings in application props

--- user/src/main/java/com/project/user/config/SecurityConfig.java
+++ user/src/main/java/com/project/user/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +19,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
